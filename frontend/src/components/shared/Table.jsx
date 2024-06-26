104671eab752942a76e79910686dc9662d8682c6
Ezra fathurrahman <muhammad.fathurrahman@phincon.com>
2024-06-26 10:44:53 +0700

    feat: update table contents and add new navigation button

--- frontend/src/components/shared/Table.jsx
+++ frontend/src/components/shared/Table.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { FaEdit } from "react-icons/fa";
 import { RiDeleteBin2Fill } from "react-icons/ri";
+import { AiOutlineUser, AiOutlineMenu, AiOutlineLogout } from 'react-icons/ai';
 
  
 export function Table() {
   return (
-    <div className="bg-white rounded-lg shadow-md overflow-hidden">
+    <div className="my-3 bg-white rounded-lg shadow-md overflow-hidden">
                     <div className="px-6 py-4 border-b border-gray-200 flex justify-between items-center">
                         <h2 className="text-xl font-semibold text-gray-800">Order Monitoring</h2>
                         <button className="text-green-500 hover:text-green-600 font-semibold transition duration-150 ease-in-out">
@@ -17,21 +18,22 @@ export function Table() {
                             <thead className="bg-gray-50">
                                 <tr>
                                     <th className=" px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">Order ID</th>
+                                    <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">Milestones</th>
                                     <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">Status</th>
-                                    <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">Customer</th>
-                                    <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">Date</th>
-                                    <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">Action</th>
+                                    <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">Last Update</th>
+                                    <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">Retry / Rollback</th>
                                 </tr>
                             </thead>
                             <tbody className="text-center bg-white divide-y divide-gray-200">
                                 <tr>
-                                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">#12345</td>
+                                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                        <a href="#" className="hover:text-cyan-600 ">       734068550</a></td>
                                     <td className="px-6 py-4 whitespace-nowrap">
-                                        <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                                            Completed
-                                        </span>
+                                    OrderOnProgress
                                     </td>
-                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">John Doe</td>
+                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500"><span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-yellow-100 text-green-800">
+                                            Init
+                                        </span></td>
                                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">2024-06-25</td>
                                     <td className="flex flex-wrap px-6 py-4 justify-evenly whitespace-nowrap text-sm font-medium">
                                     <a href="#"><FaEdit size={22}/></a>
@@ -39,13 +41,13 @@ export function Table() {
                                     </td>
                                 </tr>
                                 <tr>
-                                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">#12346</td>
+                                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900"><a href="#" className="hover:text-cyan-600 ">       734068551</a></td>
                                     <td className="px-6 py-4 whitespace-nowrap">
-                                        <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-yellow-100 text-yellow-800">
-                                            In Progress
-                                        </span>
+                                    OrderCompleted
                                     </td>
-                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">Jane Smith</td>
+                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500"><span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                                            Completed
+                                        </span></td>
                                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">2024-06-25</td>
                                     <td className="flex flex-wrap justify-evenly px-6 py-4 whitespace-nowrap text-sm font-medium inline">
                                         <a href="#"><FaEdit size={22}/></a>
@@ -53,6 +55,68 @@ export function Table() {
                                     
                                     </td>
                                 </tr>
+                                <tr>
+                                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900"><a href="#" className="hover:text-cyan-600 ">       734068552</a></td>
+                                    <td className="px-6 py-4 whitespace-nowrap">
+                                    OrderCompleted
+                                    </td>
+                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500"><span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                                            Completed
+                                        </span></td>
+                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">2024-06-25</td>
+                                    <td className="flex flex-wrap justify-evenly px-6 py-4 whitespace-nowrap text-sm font-medium inline">
+                                        <a href="#"><FaEdit size={22}/></a>
+                                        <a href="#"><RiDeleteBin2Fill size={22}/></a>
+                                    
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900"><a href="#" className="hover:text-cyan-600 ">       734068553</a></td>
+                                    <td className="px-6 py-4 whitespace-nowrap">
+                                    OrderCompleted
+                                    </td>
+                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500"><span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                                            Completed
+                                        </span></td>
+                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">2024-06-25</td>
+                                    <td className="flex flex-wrap justify-evenly px-6 py-4 whitespace-nowrap text-sm font-medium inline">
+                                        <a href="#"><FaEdit size={22}/></a>
+                                        <a href="#"><RiDeleteBin2Fill size={22}/></a>
+                                    
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900"><a href="#" className="hover:text-cyan-600 ">       734068554</a></td>
+                                    <td className="px-6 py-4 whitespace-nowrap">
+                                    OrderCompleted
+                                    </td>
+                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500"><span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                                            Completed
+                                        </span></td>
+                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">2024-06-25</td>
+                                    <td className="flex flex-wrap justify-evenly px-6 py-4 whitespace-nowrap text-sm font-medium inline">
+                                        <a href="#"><FaEdit size={22}/></a>
+                                        <a href="#"><RiDeleteBin2Fill size={22}/></a>
+                                    
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900"><a href="#" className="hover:text-cyan-600 ">       734068555</a></td>
+                                    <td className="px-6 py-4 whitespace-nowrap">
+                                    OrderCompleted
+                                    </td>
+                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500"><span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                                            Completed
+                                        </span></td>
+                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">2024-06-25</td>
+                                    <td className="flex flex-wrap justify-evenly px-6 py-4 whitespace-nowrap text-sm font-medium inline">
+                                        <a href="#"><FaEdit size={22}/></a>
+                                        <a href="#"><RiDeleteBin2Fill size={22}/></a>
+                                    
+                                    </td>
+                                </tr>
+                                
+                                
                             </tbody>
                         </table>
                     </div>
